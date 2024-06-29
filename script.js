cbbd8cd3e79005e5f60c46113bb577bbe9fa5a4e
Harshwardhan Rastogi <161843845+HarshWardhan-R@users.noreply.github.com>
2024-06-29 12:22:05 +0530

    Add files via upload
    it is for my school project, don't bother to open it

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: true
+    });
+});
+
